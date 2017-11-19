161f10f42b9fe7619881761bfeeb72e4106b30f4
Renaud Helias <renaudhelias@gmail.com>
2017-11-19 16:32:32 +0100

    JDVA#2 test

---
+++ JDVA#2_test/README.md
@@ -0,0 +1,16 @@
+# [JDVA#2] Passage en mode 0 et tracé de point lent
+# Tested by Renaud
+
+Inspiré d'une vidéo CPC de Oldschool is beautiful : http://www.youtube.com/watch?v=xFnF3tZkzSs
+
+http://www.cantrell.org.uk/david/tech/cpc/cpc-firmware/ > Graphics
+
+Je personnalise hello.c comme dans la vidéo, je patch _compil.bat pour obtenir jdva2.dsk en sortie.
+
+Je patch _compil.bat avec l'ajout du paramètre "--oldralloc" afin que "LD A,4 (IX)" fasse son effet.
+
+Je lance _compil.bat, j'obtiens jdva2.dsk
+
+![JDVA2.dsk.png](JDVA2.dsk.png)
+
+Je test : ça marche :)
---
+++ JDVA#2_test/hello.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void pen(unsigned char p)
+{
+	__asm
+		ld A,4 (IX)
+		call #0xBBDE
+	__endasm;
+}
+
+// char --> -128 +128
+// unsigned --> char 0..255
+void mode(unsigned char m)
+{
+	__asm
+		ld A,4 (IX)
+		call #0xBC0E
+	__endasm;
+}
+
+void mode2(void)
+{
+	__asm
+		ld A,#2
+		call #0xBC0E
+	__endasm;
+}
+
+// int : 16bit
+void plot(unsigned int x,unsigned int y)
+{
+	__asm
+		// DE (little-endian :p)
+		ld E,4 (IX)
+		ld D,5 (IX)
+		// HL (little-endian :p)
+		ld L,6 (IX)
+		ld H,7 (IX)
+		call #0xBBEA
+	__endasm;
+}
+
+void main(void)
+{
+	mode2();
+	mode(0);
+	printf("Hello World !");
+	pen(1);
+	plot(0,0);
+	pen(2);
+	// 160*200
+	plot(159,199);
+	// 640*400
+	plot(639,399);
+	pen(3);
+	plot(1,4);
+	plot(2,8);
+	plot(3,12);
+	plot(4,16);
+	while(1){}
+}
