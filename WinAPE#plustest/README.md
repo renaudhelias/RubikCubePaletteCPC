7ebf4fb5f22f1234bdf12d68a7df742e8b68442b
Renaud Helias <renaudhelias@gmail.com>
2017-11-19 14:49:33 +0100

    WinAPE plustest (compiling plustest.asm)

---
+++ WinAPE#plustest/README.md
@@ -0,0 +1,34 @@
+# [WinAPE#plustest] WinAPE plustest.asm to plustest.dsk
+
+WinAPE comprend un assembleur Maxam.
+
+http://www.winape.net/downloads.jsp :
+- Plus Test Source : plustest.asm
+- ASM Library : asic.asm, firmware.asm, inks.asm, keys.asm
+
+
+Si j'utilise Maxam directement sur mon CPC pour compiler plustest.asm :
+- ROM Maxam insérré (en emplacement 05)
+- ùmaxam
+- T (Text editor)
+- L (Load)
+- plustest.asm (Enter filename:)
+- A (Assemble text)
+- ** FATAL ERROR ** Out of memory
+
+Si j'utilise WinAPE (sans ROM Maxam) :
+- menu File > Drive A > New Blank Disc
+- menu File > Drive A > Format disk "DATA (SS 40)"
+- menu Assembler > Show Assembler
+	- J'ouvre plustest.asm
+	- menu Assemble > Assemble
+	- menu Assemble > Run
+- Là l'application se lance avec écrit en haut "Select a test to run".
+	- Je reviens dans l'Assembler, j'ajoute en première ligne les trois lignes suivantes :
+		-org &8000
+		-run &8000
+		-write "plustest.bin"
+	-menu Assemble > Assemble
+- cat
+
+Voilà :)
---
+++ WinAPE#plustest/keys.asm
@@ -0,0 +1,80 @@
+.KEY_UP        equ #00	; Row 0 - bit 0
+.KEY_RIGHT     equ #01	; 1
+.KEY_DOWN      equ #02	; 2
+.KEY_F9        equ #03	; 3
+.KEY_F6        equ #04	; 4
+.KEY_F3        equ #05	; 5
+.KEY_ENTER     equ #06	; 6
+.KEY_DECIMAL   equ #07	; 7
+.KEY_LEFT      equ #08	; Row 1 - bit 0
+.KEY_COPY      equ #09	; 1
+.KEY_F7        equ #0a
+.KEY_F8        equ #0b
+.KEY_F5        equ #0c
+.KEY_F1        equ #0d
+.KEY_F2        equ #0e
+.KEY_F0        equ #0f
+.KEY_CLR       equ #10	; Row 2
+.KEY_LSQUARE   equ #11
+.KEY_RETURN    equ #12
+.KEY_RSQUARE   equ #13
+.KEY_F4        equ #14
+.KEY_SHIFT     equ #15
+.KEY_BACKSLASH equ #16
+.KEY_CONTROL   equ #17
+.KEY_UPARROW   equ #18	; Row 3
+.KEY_MINUS     equ #19
+.KEY_AT        equ #1a
+.KEY_P         equ #1b
+.KEY_SEMICOLON equ #1c
+.KEY_COLON     equ #1d
+.KEY_SLASH     equ #1e
+.KEY_POINT     equ #1f
+.KEY_0         equ #20	; Row 4
+.KEY_9         equ #21
+.KEY_O         equ #22
+.KEY_I         equ #23
+.KEY_L         equ #24
+.KEY_K         equ #25
+.KEY_M         equ #26
+.KEY_COMMA     equ #27
+.KEY_8         equ #28	; Row 5
+.KEY_7         equ #29
+.KEY_U         equ #2a
+.KEY_Y         equ #2b
+.KEY_H         equ #2c
+.KEY_J         equ #2d
+.KEY_N         equ #2e
+.KEY_SPACE     equ #2f
+.KEY_6         equ #30	; Row 6
+.KEY_5         equ #31
+.KEY_R         equ #32
+.KEY_T         equ #33
+.KEY_G         equ #34
+.KEY_F         equ #35
+.KEY_B         equ #36
+.KEY_V         equ #37
+.KEY_4         equ #38	; Row 7
+.KEY_3         equ #39
+.KEY_E         equ #3a
+.KEY_W         equ #3b
+.KEY_S         equ #3c
+.KEY_D         equ #3d
+.KEY_C         equ #3e
+.KEY_X         equ #3f
+.KEY_1         equ #40	; Row 8
+.KEY_2         equ #41
+.KEY_ESC       equ #42
+.KEY_Q         equ #43
+.KEY_TAB       equ #44
+.KEY_A         equ #45
+.KEY_CAPSLOCK  equ #46
+.KEY_Z         equ #47
+.KEY_JOY_UP    equ #48	; Row 9
+.KEY_JOY_DOWN  equ #49
+.KEY_JOY_LEFT  equ #4a
+.KEY_JOY_RIGHT equ #4b
+.KEY_JOY_FIRE1 equ #4c
+.KEY_JOY_FIRE2 equ #4d
+.KEY_JOY_FIRE3 equ #4e
+.KEY_DEL       equ #4f
