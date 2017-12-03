cb10f10f59e2a5f31561350084957d7a7c87d12d
Renaud Helias <renaudhelias@gmail.com>
2017-12-03 14:24:41 +0100

    move, line

--- JDVA#2_test/hello.c
+++ JDVA#2_test/hello.c
@@ -41,6 +41,32 @@ void plot(unsigned int x,unsigned int y)
 	__endasm;
 }
 
+void move(unsigned int x,unsigned int y)
+{
+	__asm
+		// DE (little-endian :p)
+		ld E,4 (IX)
+		ld D,5 (IX)
+		// HL (little-endian :p)
+		ld L,6 (IX)
+		ld H,7 (IX)
+		call #0xBBC0
+	__endasm;
+}
+
+void line(unsigned int x,unsigned int y)
+{
+	__asm
+		// DE (little-endian :p)
+		ld E,4 (IX)
+		ld D,5 (IX)
+		// HL (little-endian :p)
+		ld L,6 (IX)
+		ld H,7 (IX)
+		call #0xBBF6
+	__endasm;
+}
+
 void main(void)
 {
 	mode2();
@@ -58,5 +84,8 @@ void main(void)
 	plot(2,8);
 	plot(3,12);
 	plot(4,16);
+	pen(4);
+	move(10,0);
+	line(100,100);
 	while(1){}
 }
