795234e53870166f2035ce9a61799d0898302a0d
Renaud Helias <renaudhelias@gmail.com>
2018-01-19 19:25:27 +0100

    JDVA#6 poulpe qui bouge

--- JDVA#6_test/main.c
+++ JDVA#6_test/main.c
@@ -11,12 +11,15 @@ unsigned char *fish[12];
 
 void main(void)
 {
+	char x;
+	x=10;
+	
 	fish[0]=poisson1;
 	fish[1]=poisson2;
 	fish[2]=poisson_groupe;
 	fish[3]=poisson_groupe+(6*16);
 	fish[4]=poisson_groupe+((6*16)*2);
-	
+	fish[5]=poisson_groupe+((6*16)*9); // fond vide
 	
 	mode(0);
 	set_palette(poisson_palette);
@@ -24,12 +27,22 @@ void main(void)
 
 	while(1){
 		check_controller();
-		if (get_key(Key_A) || get_key(Key_Space)) {
-			
-			put_frame(vram(10,100),6,16,fish[1]);
-			
-			
+		
+//		if (get_key(Key_A) || get_key(Key_Space)) {
+//			put_frame(vram(10,100),6,16,fish[1]);
+//		}
+		
+		// synchro en bas de l'écran
+		vsync();
+		// effacer poisson
+		put_frame(vram(x,100),6,16,fish[5]);
+		if (get_key(Key_O)) {
+			x--;
+		}		
+		if (get_key(Key_P)) {
+			x++;
 		}
-			
+		// afficher poisson
+		put_frame(vram(x,100),6,16,fish[1]);
 	}
 }
