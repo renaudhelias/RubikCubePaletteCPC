f54fd6ff142956761e80f3766acba6a2ae7177c9
Renaud Helias <renaudhelias@gmail.com>
2018-03-03 18:48:33 +0100

    sound FX

--- JDVA#6_test/_compil.bat
+++ JDVA#6_test/_compil.bat
@@ -17,7 +17,7 @@ sdcc -mz80 --code-loc 0x0138 --data-loc 0 --oldralloc --no-std-crt0 crt0_cpc.rel
 sdcc -mz80 --code-loc 0x0138 --data-loc 0 --oldralloc --no-std-crt0 crt0_cpc.rel putchar_cpc.rel jdvapi_basic.rel jdvapi_frame.rel jdvapi_keyb.rel jdvapi_sync.rel joueurs.rel combat.c
 sdcc -mz80 --code-loc 0x0138 --data-loc 0 --fno-omit-frame-pointer --oldralloc --no-std-crt0 crt0_cpc.rel putchar_cpc.rel jdvapi_basic.rel jdvapi_frame.rel jdvapi_keyb.rel jdvapi_sync.rel combat2.c
 sdcc -mz80 --code-loc 0x0138 --data-loc 0 --oldralloc --no-std-crt0 crt0_cpc.rel putchar_cpc.rel jdvapi_basic.rel jdvapi_sync.rel raster.c
-sdcc -mz80 --code-loc 0x0138 --data-loc 0 --oldralloc --no-std-crt0 crt0_cpc.rel putchar_cpc.rel jdvapi_basic.rel jdvapi_sync.rel arkostracker.rel molusk.rel musique.c
+sdcc -mz80 --code-loc 0x0138 --data-loc 0 --oldralloc --no-std-crt0 crt0_cpc.rel putchar_cpc.rel jdvapi_basic.rel jdvapi_keyb.rel jdvapi_sync.rel arkostracker.rel molusk.rel musique.c
 sdcc -mz80 --code-loc 0x04038 --data-loc 0 --fno-omit-frame-pointer --oldralloc --no-std-crt0 crt0_cpc.rel putchar_cpc.rel Load01.c
 sdcc -mz80 --code-loc 0x04038 --data-loc 0 --fno-omit-frame-pointer --oldralloc --no-std-crt0 crt0_cpc.rel putchar_cpc.rel Load02.c
 hex2bin main.ihx
--- JDVA#6_test/musique.c
+++ JDVA#6_test/musique.c
@@ -3,6 +3,7 @@
 
 #include "jdvapi_basic.h"
 #include "jdvapi_sync.h"
+#include "jdvapi_keyb.h"
 // CPCTelera/src/audio/audio.h
 #include "arkostracker.h"
 // cpctelera-1.4.2/examples/medium/arkosAudio
@@ -32,19 +33,73 @@ void border_raster_end()
   __endasm;
 }
 
+void border_raster_end2()
+{
+  //grimware/official.sks.player.1.2.zip/exemple.asm
+  __asm
+	; Set the BORDER to un peu de violet...
+	ld bc,#0x7F5D
+	out (c),c
+  __endasm;
+}
+
 void main(void)
 {
+	unsigned char playing=1;
+	unsigned char touche_Space=0;
+	unsigned char touche_O=0;
+	unsigned char touche_P=0;
 	raster_halt();
 	cpct_akp_musicInit(molusk_song);
+	cpct_akp_SFXInit(molusk_song);
 	
 	while (1) {
+		check_controller();
+		
 		vsync();
 		halt();
 		halt();
 		halt();
 		border_raster_begin();
-		// Play the STarKos song
-		cpct_akp_musicPlay();
-		border_raster_end();
+		if (playing) {
+			// Play the STarKos song
+			cpct_akp_musicPlay();
+			
+			if (cpct_akp_songLoopTimes > 0) {
+				// Song has ended, start it again and set loop to 0
+				cpct_akp_musicInit(molusk_song);
+			}
+		}
+		
+		// Key_Space
+		if (!touche_Space && get_key(Key_Space)) {
+			touche_Space=1;
+			if (playing) {
+				cpct_akp_stop(); // does cut also FX :/
+				playing=0;
+			} else {
+				playing=1;
+			}
+		} else if (!get_key(Key_Space)) {
+			touche_Space=0;
+		}
+		if (!touche_O && get_key(Key_O)) {
+			touche_O=1;
+			cpct_akp_SFXPlay(13, 15, 36, 20, 0, AY_CHANNEL_A);
+		} else if (!get_key(Key_O)) {
+			touche_O=0;
+		}
+		if (!touche_P && get_key(Key_P)) {
+			touche_P=1;
+			cpct_akp_SFXPlay(3, 15, 60, 0, 40, AY_CHANNEL_C);
+		} else if (!get_key(Key_P)) {
+			touche_P=0;
+		}
+		
+		if (get_key(Key_O) || get_key(Key_P) || get_key(Key_Space)) {
+			border_raster_end2();
+		} else {
+			border_raster_end();
+		}
 	}
 }
