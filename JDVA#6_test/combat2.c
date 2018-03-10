ad02326b127fa8ca7697618cacab3fed96b51b9b
Renaud Helias <renaudhelias@gmail.com>
2018-03-10 18:26:15 +0100

    combat 2 with some background sound

--- JDVA#6_test/combat2.c
+++ JDVA#6_test/combat2.c
@@ -21,6 +21,131 @@
 unsigned int *vram;
 
 
+void cpct_akp_musicInit()
+{
+	__asm
+	;; backup Z80 state
+  push af
+  push bc
+  push de
+  push hl
+  push ix
+  push iy
+  exx
+  ex af, af'
+  push af
+  push bc
+  push de
+  push hl
+  
+	; sks2000.bin/exemple.asm
+	ld de,#0x3000
+	call #0x2000
+	
+	;; restore Z80 state
+  pop hl
+  pop de
+  pop bc
+  pop af
+  ex af, af'
+  exx
+  pop iy
+  pop ix
+  pop hl
+  pop de
+  pop bc
+  pop af
+	__endasm;
+}
+
+void cpct_akp_musicPlay()
+{
+	__asm
+	;; backup Z80 state
+  push af
+  push bc
+  push de
+  push hl
+  push ix
+  push iy
+  exx
+  ex af, af'
+  push af
+  push bc
+  push de
+  push hl
+	
+	; sks2000.bin/exemple.asm
+	call #0x2003
+	
+	;; restore Z80 state
+  pop hl
+  pop de
+  pop bc
+  pop af
+  ex af, af'
+  exx
+  pop iy
+  pop ix
+  pop hl
+  pop de
+  pop bc
+  pop af
+	__endasm;
+}
+
+/*
+void border_raster_begin()
+{
+  //grimware/official.sks.player.1.2.zip/exemple.asm
+  __asm
+	; Set a white raster in the BORDER to "see" how much
+	; CPU time the player takes.
+	ld bc,#0x7F10
+	out (c),c
+	ld c,#0x4B
+	out (c),c
+  __endasm;
+}
+
+void border_raster_begin2()
+{
+  //grimware/official.sks.player.1.2.zip/exemple.asm
+  __asm
+	; Set the BORDER to un peu de violet...
+	ld bc,#0x7F5D
+	out (c),c
+  __endasm;
+}
+
+void border_raster_end()
+{
+  //grimware/official.sks.player.1.2.zip/exemple.asm
+  __asm
+	; Set the BORDER to black
+	ld bc,#0x7F54
+	out (c),c
+  __endasm;
+}
+*/
+
+unsigned int is_vsync;
+
+void callback_roulette(unsigned char roulette)
+{
+	if (roulette==0) {
+		//border_raster_begin();
+		
+		// Play the STarKos song
+		cpct_akp_musicPlay();
+		
+		//border_raster_begin2();
+		
+		is_vsync=1;
+	}
+	return;
+}
+
 
 void transfertEtDecoupe()
 {
@@ -108,18 +233,30 @@ calqueC000();
 	// fond
 	erase_frame((unsigned char *)(vram[120]+3),6*8+3,50);
 
+	vsync();
+	handle_raster(callback_roulette);
+	raster();
+	
+	// cpctelera-1.4.2/examples/medium/arkosAudio
+	cpct_akp_musicInit(); //(void *)0x4000);
+
 	// faire une boucle qui :
 	while(1){
 		
 	// affiche C000 pendant qu'on recopie de C000 vers 4000 la "zone de combat"
-	vsync();
+	if (is_vsync==0) {}
+	is_vsync=0;
+
 	calqueC000();
 	bank0123();
 	memcpy((char *)0x4000, (char *)0xC000, 0x3FFF); // memcpy(destination,source,longueur)
-
+	
+	//border_raster_end();
 		
 	// affiche 4000 pendant qu'on pose deux sprites de 4000 vers C000
-	vsync();
+	if (is_vsync==0) {}
+	is_vsync=0;
+
 	calque4000();
 	bank4_4000();
 	x=7;
@@ -146,6 +283,7 @@ calqueC000();
 		//z=0;
 	//}
 	
+	//border_raster_end();
 	}
 	
 }
