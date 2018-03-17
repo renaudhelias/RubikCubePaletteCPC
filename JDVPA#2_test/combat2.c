921496e688b3eac908979f03385864ad29140726
Renaud Helias <renaudhelias@gmail.com>
2018-03-17 19:40:18 +0100

    JDVPA#2 combat2, du surplace pour le moment...

--- JDVPA#2_test/combat2.c
+++ JDVPA#2_test/combat2.c
@@ -310,11 +310,182 @@ const struct CALQUE_J2R J2R= {
 	.flaque={45,7}
 };
 
+#define BANK_4 4
+#define BANK_5 5
+#define BANK_6 6
+#define BANK_7 7
+
+#define ALLEZ 1
+#define RETOUR 2
+#define MARCHE_AVANT 3
+#define MARCHE_ARRIERE 4
+
+#define PERSO_LIU_KANG 1
+#define PERSO_SUB_ZERO 2
+
+typedef struct {
+	char perso; // identifiant du jouer
+	char direction;
+	CALQUE animation;
+	char anim_restant; // decompte un CALQUE.l
+	char allez_retour; // si ALLEZ ou RETOUR, decompte aussi un allez-retour de l'anim (ALLEZ, puis RETOUR, puis 0)
+	char bank; // la bank mémoire de l'animation en cours
+	char x;
+} ANIMATION;
+
+#define DIRECTION_DROITE 1
+#define DIRECTION_GAUCHE 2
+#define DIRECTION_HAUT 4
+#define DIRECTION_BAS 8
+#define DIRECTION_FIRE 16
+
+ANIMATION liu_kang;
+ANIMATION sub_zero;
+
+#define DEPLACEMENT_AVANCE 1
+#define DEPLACEMENT_RECULE 2
+#define DEPLACEMENT_AUCUNE 3
+
+void action(ANIMATION * joueur, char direction_pressed) {
+	char deplacement=0;
+	// FIXME : si je MARCHE, alors je peux lancer une nouvelle action.
+	// en fait quand tu attend, tu MARCHE sur place entre deux états (d'un air menaçant)
+	
+	if (joueur->allez_retour == ALLEZ || (joueur->allez_retour!=RETOUR && joueur->anim_restant > 0)) {
+		// une animation ALLEZ est en cours
+		if (joueur->allez_retour == MARCHE_AVANT || joueur->allez_retour == MARCHE_ARRIERE) {
+			// déplacement
+			if (joueur->direction & DIRECTION_DROITE != 0) {
+				// le joueur va a droite
+				joueur->x = joueur->x + 1;
+				if (joueur->x > 100) {
+					joueur->x=0;
+				}
+			}
+			if (joueur->direction & DIRECTION_GAUCHE != 0) {
+				// le joueur va a gauche
+				joueur->x = joueur->x - 1;
+				if (joueur->x < 1) {
+					joueur->x=100;
+				}
+			}
+		}
+		if (joueur->anim_restant > 0) {
+			joueur->anim_restant = joueur->anim_restant -1;
+			if (joueur->anim_restant == 0 && joueur->allez_retour==ALLEZ) {
+				joueur->anim_restant = joueur->animation.l;
+				joueur->allez_retour = RETOUR;
+			}
+		}
+	} else if (joueur->allez_retour == RETOUR) {
+		// une animation RETOUR est en cours
+		if (joueur->anim_restant > 0) {
+			joueur->anim_restant = joueur->anim_restant -1;
+			if (joueur->anim_restant == 0) {
+				joueur->allez_retour = 0;
+			}
+		}
+	} else {
+		
+		// déclanchement d'une nouvelle animation
+		joueur->direction=direction_pressed;
+		if ((joueur->direction & DIRECTION_DROITE != 0) && (joueur->direction & DIRECTION_GAUCHE != 0)) {
+			// un idiot
+			deplacement = DEPLACEMENT_AUCUNE;
+		} else if (joueur->direction & DIRECTION_DROITE != 0) {
+			if (joueur->perso==PERSO_LIU_KANG) {
+				deplacement = DEPLACEMENT_AVANCE;
+			} else {
+				deplacement = DEPLACEMENT_RECULE;
+			}
+		} else if (joueur->direction & DIRECTION_GAUCHE != 0) {
+			if (joueur->perso==PERSO_SUB_ZERO) {
+				deplacement = DEPLACEMENT_AVANCE;
+			} else {
+				deplacement = DEPLACEMENT_RECULE;
+			}
+		} else {
+			deplacement = DEPLACEMENT_AUCUNE;
+		}
+		
+		if (joueur->perso == PERSO_LIU_KANG) {
+			if (deplacement == DEPLACEMENT_AVANCE) {
+				joueur->animation.o=J1A.marcher.o;
+				joueur->animation.l=J1A.marcher.l;
+				joueur->bank=BANK_4;
+				joueur->anim_restant=joueur->animation.l;
+				joueur->allez_retour=MARCHE_AVANT;
+			} else if (deplacement == DEPLACEMENT_RECULE) {
+				joueur->animation.o=J1A.marcher.o;
+				joueur->animation.l=J1A.marcher.l;
+				joueur->bank=BANK_4;
+				joueur->anim_restant=joueur->animation.l;
+				joueur->allez_retour=MARCHE_ARRIERE;
+			} else {
+				joueur->animation.o=J1A.marcher.o;
+				joueur->animation.l=J1A.marcher.l;
+				joueur->bank=BANK_4;
+				joueur->anim_restant=2; // sur place, mais pas totalement fixe : mode faché.
+				joueur->allez_retour=0;
+			}
+		} else {
+			if (deplacement == DEPLACEMENT_AVANCE) {
+				joueur->animation.o=J2A.marcher.o;
+				joueur->animation.l=J2A.marcher.l;
+				joueur->bank=BANK_6;
+				joueur->anim_restant=joueur->animation.l;
+				joueur->allez_retour=MARCHE_AVANT;
+			} else if (deplacement == DEPLACEMENT_RECULE) {
+				joueur->animation.o=J2A.marcher.o;
+				joueur->animation.l=J2A.marcher.l;
+				joueur->bank=BANK_6;
+				joueur->anim_restant=joueur->animation.l;
+				joueur->allez_retour=MARCHE_ARRIERE;
+			} else {
+				joueur->animation.o=J2A.marcher.o;
+				joueur->animation.l=J2A.marcher.l;
+				joueur->bank=BANK_6;
+				joueur->anim_restant=2; // sur place, mais pas totalement fixe : mode faché.
+				joueur->allez_retour=0;
+			}
+		}
+	}
+}
+
+void switch_bank(ANIMATION * joueur) {
+	switch (joueur->bank) {
+		case BANK_4 :
+			bank4_4000();
+		return;
+		case BANK_5 :
+			bank5_4000();
+		return;
+		case BANK_6 :
+			bank6_4000();
+		return;
+		case BANK_7 :
+			bank7_4000();
+		return;
+	}
+}
+
 void main(void)
 {
+	//init liu_kang et sub_zero
+	liu_kang.x=10;
+	liu_kang.perso=PERSO_LIU_KANG;
+	liu_kang.anim_restant=0;
+	liu_kang.allez_retour=0;
+	
+	sub_zero.x=60;
+	sub_zero.perso=PERSO_SUB_ZERO;
+	sub_zero.anim_restant=0;
+	sub_zero.allez_retour=0;
+	
+	
 	// against "so said EVELYN the modified DOG" => volatile
-	volatile char layer=0;volatile char x=10;//char z=0;
-	char aaah=3;
+	// volatile char layer=0;volatile char x=10;//char z=0;
+	// char aaah=3;
 
 	//intro en &4000
 	SetupDOS();
@@ -345,17 +516,15 @@ void main(void)
 	transfertEtDecoupe();
 
 	//if (J2R.hadouken2_personnage_patch.l == aaah) { // volatiles... sucks ?
-	if (J2R.hadouken2_personnage_patch.l == aaah) {
-		bank0123();
-		LoadFile("J2A.scr", (char *)0xC000); // un scr exporté "linéaire"
-		bank6_4000();
-		transfertEtDecoupe();
-		
-		bank0123();
-		LoadFile("J2R.scr", (char *)0xC000); // un scr exporté "linéaire"
-		bank7_4000();
-		transfertEtDecoupe();
-	}
+	bank0123();
+	LoadFile("J2A.scr", (char *)0xC000); // un scr exporté "linéaire"
+	bank6_4000();
+	transfertEtDecoupe();
+
+	bank0123();
+	LoadFile("J2R.scr", (char *)0xC000); // un scr exporté "linéaire"
+	bank7_4000();
+	transfertEtDecoupe();
 
 // et finalement.
 calqueC000();
@@ -394,31 +563,15 @@ calqueC000();
 	is_vsync=0;
 
 	calque4000();
-	bank4_4000();
-	x=7;
-	// gauche 1
-	put_frame((unsigned char *)(vram[120]+x),6,50,0x4000+((6*50)*layer));
-	// droite 1
-	put_frame((unsigned char *)(vram[120]+x+6+6+6+6+3),6,50,0x4000+((6*50)*layer));
-	bank6_4000();
-	// gauche 2
-	erase_frame((unsigned char *)(vram[120]+x+6),4,50);
-	put_frame_transparent((unsigned char *)(vram[120]+x+3+1),6,50,0x4000+((6*50)*layer));
-	// droite 2
-	put_frame((unsigned char *)(vram[120]+x+6+6+6+6+6+3),6,50,0x4000+((6*50)*layer));
-	bank5_4000();
-	// milieu 1
-	put_frame((unsigned char *)(vram[120]+x+6+6),6,50,0x4000+((6*50)*layer));
-	bank7_4000();
-	// milieu 2
-	put_frame((unsigned char *)(vram[120]+x+6+6+6),6,50,0x4000+((6*50)*layer));
-	//z++;
-	// 21 secondes avec 50*3
-	//if (z>3) {
-		layer=(layer+1)%(13*4);
-		//z=0;
-	//}
 	
+	action(&liu_kang,DIRECTION_DROITE | DIRECTION_HAUT | DIRECTION_FIRE);
+	action(&sub_zero,DIRECTION_GAUCHE | DIRECTION_FIRE);
+	
+	switch_bank(&liu_kang);
+	put_frame((unsigned char *)(vram[120]+liu_kang.x),6,50,0x4000+((6*50)*(liu_kang.animation.o+liu_kang.anim_restant)));
+	switch_bank(&sub_zero);
+	put_frame((unsigned char *)(vram[120]+sub_zero.x),6,50,0x4000+((6*50)*(sub_zero.animation.o+sub_zero.anim_restant)));
+
 	//border_raster_end();
 	}
 	
