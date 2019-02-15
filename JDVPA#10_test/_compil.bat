0a2aba34251ca8a63ecadb530bf116f258fe0864
Renaud Helias <renaudhelias@gmail.com>
2019-02-15 11:02:23 +0100

    jdvpa 10 sprite_collision

--- JDVPA#10_test/_compil.bat
+++ JDVPA#10_test/_compil.bat
@@ -17,10 +17,11 @@ sdcc -mz80 -c --std-c99 --opt-code-speed ghost_reduce_sprites.c
 sdcc -mz80 -c --std-c99 --opt-code-speed blank_reduce_sprites.c
 sdcc -mz80 -c --std-c99 --opt-code-speed player.c
 sdcc -mz80 -c --std-c99 --opt-code-speed ghost.c
+sdcc -mz80 -c --std-c99 --opt-code-speed sprite_collision.c
 sdcc -mz80 -c --std-c99 --opt-code-speed tiles.c
 sdcc -mz80 -c --std-c99 --opt-code-speed labyPac_map.c
 sdcc -mz80 -c --std-c99 --opt-code-speed labyPac_map.rel laby_data.c
-sdcc -mz80 --code-loc 0x0138 --data-loc 0 --no-std-crt0 jdvapi_basic1.rel jdvapi_keyb.rel jdvapi_frame.rel jdvapi_sync.rel crt0_cpc.rel putchar_cpc.rel pacman_reduce_sprites.rel ghost_reduce_sprites.rel blank_reduce_sprites.rel player.rel ghost.rel tiles.rel labyPac_map.rel laby_data.rel main.c
+sdcc -mz80 --code-loc 0x0138 --data-loc 0 --no-std-crt0 jdvapi_basic1.rel jdvapi_keyb.rel jdvapi_frame.rel jdvapi_sync.rel crt0_cpc.rel putchar_cpc.rel pacman_reduce_sprites.rel ghost_reduce_sprites.rel blank_reduce_sprites.rel player.rel ghost.rel sprite_collision.rel tiles.rel labyPac_map.rel laby_data.rel main.c
 hex2bin main.ihx
 CPCDiskXP -File main.bin -AddAmsdosHeader 100 -AddToNewDsk jdvpa10.dsk
 pause
