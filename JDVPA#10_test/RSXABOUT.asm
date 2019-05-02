3c7fbcfd683f4cdff07a6099c2c510bb296f2617
Renaud Helias <renaudhelias@gmail.com>
2019-05-02 15:38:22 +0200

    Intégration de jdvapi_basic1.c dans RSXABOUT.ROM

--- JDVPA#10_test/RSXABOUT.asm
+++ JDVPA#10_test/RSXABOUT.asm
@@ -4,9 +4,15 @@ org #c000
              defw RSX_TABLE      ; Adresses des noms des Commandes externes
              JP INIT_AZERTY      ; Adresse d'execution lors du demarrage
              jp ABOUT
+			 jp BORDER
+			 jp SET_COLOR
+			 jp MODE
 RSX_TABLE:
              defb "RENAU","D"+#80
              defb "ABOU","T"+#80
+			 defb "BORDE","R"+#80
+			 defb "SETCOLO","R"+#80
+			 defb "MOD","E"+#80
              defb 0 ; FIN
 INIT_AZERTY:
              push hl
@@ -27,10 +33,26 @@ textout:
              or a
              jr nz,textout
              ret
+
+BORDER: ; void border(unsigned char nColorIndex)
+    ;ld b, 4 (ix)
+	ld b, (ix+0)
+    ld c, b
+    call #BC38 ;SCR SET INK
+ret
+SET_COLOR: ; void set_color(unsigned char nColorIndex, unsigned char nPaletteIndex)
+    ld a, (ix+0)
+    ld b, (ix+2)
+    ld c, b
+    call #BC32 ;SCR SET INK
+ret
+MODE:
+	ld A, (IX+0)
+	call #BC0E
+ret
+
 ABOUT_TEXT1:
 defb " Renaud test RSX ABOUT,",10,13
 defb "      tester aussi HELP",10,13
 defb 10,13
 defb 0 ; 7 ça fait beep en fait ^^
-
-
